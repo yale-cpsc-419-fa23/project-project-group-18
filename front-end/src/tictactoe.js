3dcb89d8c403a8a68c552e38062d2bd3ffcfcf1d
严昕辰 <3190103296@zju.edu.cn>
2023-11-16 16:21:42 -0500

    update frontend

--- front-end/src/tictactoe.js
+++ front-end/src/tictactoe.js
@@ -1,10 +1,11 @@
 import {SERVER_ADDRESS} from './global.js'
 
 var socket;
-var currentPiece, opponentPiece;
+var currentPiece;
 var player_id, room_id;
-var cells = [], pieces = [];
+var cells = [];
 var checkerboardTask, circleTask, crossTask;
+var turn = false;
 
 function get_cookie(name) {
     // get cookie by name
@@ -37,14 +38,11 @@ function run() {
 
 function make_move(index) {
     disable_cell_click_events();
-    console.log(index)
     socket.emit('makemove', {player_id: player_id, room_id: room_id, index: index});
 }
 
 function update_state(data) {
-    const index = data.index;
-    $('#board').children().eq(index).text(data.turn);
-    $('#board').children().eq(index).off('click');
+    placePiece(data.index, data.turn);
 }
 
 function end_game(data) {
@@ -65,15 +63,11 @@ function leave_room() {
 }
 
 function disable_cell_click_events() {
-    $('.cell').each(function() {
-        $(this).addClass('disabled');
-    });
+    turn = false;
 }
 
 function enable_cell_click_events() {
-    $('.cell').each(function() {
-        $(this).removeClass('disabled');
-    });
+    turn = true;
 }
 
 function findClosestCell(point) {
@@ -93,28 +87,34 @@ function findClosestCell(point) {
     };
 }
 
-function placePiece(position) {
-    var circle = circleTask.loadedMeshes[0].getChildren()[0].createInstance();
-    circle.position = position;
-    circle.scaling.x *= 0.6;
-    circle.scaling.y *= 0.3;
-    circle.scaling.z *= 0.6;
-    circle.setEnabled(true);
-    var cross = crossTask.loadedMeshes[0].getChildren()[0].createInstance();
-    cross.position = position;
-    cross.scaling.x *= 0.8;
-    cross.scaling.y *= 0.8;
-    cross.scaling.z *= 0.3;
-    cross.rotationQuaternion = null;
-    cross.rotation.x = BABYLON.Tools.ToRadians(90);
-    cross.rotation.z = BABYLON.Tools.ToRadians(45);
-    pieces.push(circle);
-    pieces.push(cross);
+function placePiece(idx, piece) {
+    var position = cells[idx].position;
+    cells[idx].occupied = true;
+    if (piece == 'O')
+    {
+        var circle = circleTask.loadedMeshes[0].getChildren()[0].createInstance();
+        circle.position = position;
+        circle.scaling.x *= 0.6;
+        circle.scaling.y *= 0.3;
+        circle.scaling.z *= 0.6;
+        circle.setEnabled(true);
+    }
+    else if (piece == 'X')
+    {
+        var cross = crossTask.loadedMeshes[0].getChildren()[0].createInstance();
+        cross.position = position;
+        cross.scaling.x *= 0.8;
+        cross.scaling.y *= 0.8;
+        cross.scaling.z *= 0.3;
+        cross.rotationQuaternion = null;
+        cross.rotation.x = BABYLON.Tools.ToRadians(90);
+        cross.rotation.z = BABYLON.Tools.ToRadians(45);
+    }
 }
 
 function createScene(canvas, engine) {
-    // Creates a basic Babylon Scene object
     var scene = new BABYLON.Scene(engine);
+    
     scene.environmentTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("https://raw.githubusercontent.com/yale-cpsc-419-fa23/project-project-group-18/mvp/front-end/models/textures/environment.env", scene);
     const skydome = BABYLON.MeshBuilder.CreateBox("sky", { size: 1000, sideOrientation: BABYLON.Mesh.BACKSIDE }, scene);
     skydome.position.y = 500;
@@ -127,33 +127,17 @@ function createScene(canvas, engine) {
     sky.projectedGroundRadius = 20;
     sky.projectedGroundHeight = 3;
     skydome.material = sky;
-    // Creates and positions a free camera
+    
     var camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
     // camera.lowerAlphaLimit = 0
     // camera.upperAlphaLimit = 0
+    camera.beta = BABYLON.Tools.ToRadians(60)
     camera.lowerBetaLimit = BABYLON.Tools.ToRadians(0)
     camera.upperBetaLimit = BABYLON.Tools.ToRadians(80)
     camera.lowerRadiusLimit = 8
     camera.upperRadiusLimit = 15
-    
-    // Targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
-    // This attaches the camera to the canvas
     camera.attachControl(canvas, false, false);
-    
-    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-
-    var text1 = new BABYLON.GUI.TextBlock();
-    text1.text = "Display room status";
-    text1.color = "white";
-    text1.fontSize = 24;
-    advancedTexture.addControl(text1);
-    var button = BABYLON.GUI.Button.CreateSimpleButton("but", "Click Me");
-    button.width = 0.2;
-    button.height = "40px";
-    button.color = "white";
-    button.background = "green";
-    advancedTexture.addControl(button);
 
     var assetsManager = new BABYLON.AssetsManager(scene);
     checkerboardTask = assetsManager.addMeshTask("checkboard", "", "https://raw.githubusercontent.com/yale-cpsc-419-fa23/project-project-group-18/mvp/front-end/models/", "checkerboard.glb");
@@ -189,13 +173,13 @@ function createScene(canvas, engine) {
     }
     assetsManager.onFinish = function (tasks) {
         canvas.addEventListener("click", function (event) {
+            if (!turn)
+                return;
             var pickResult = scene.pick(scene.pointerX, scene.pointerY);
-            if (pickResult.hit && pickResult.pickedMesh && pickResult.pickedMesh.name === "立方体") {
+            if (pickResult.hit && pickResult.pickedMesh && pickResult.pickedMesh.name === "Checkerboard") {
                 const closestCell = findClosestCell(pickResult.pickedPoint);
-                if (!closestCell.occupied) {
-                    placePiece(cells[closestCell.index].position);
-                    cells[closestCell.index].occupied = true;
-                }
+                if (!closestCell.occupied)
+                    make_move(closestCell.index)
             }
         });
         engine.runRenderLoop(function () {
@@ -211,12 +195,11 @@ function createScene(canvas, engine) {
 
 function initialize() {
     const canvas = document.getElementById('renderCanvas');
-    const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+    const engine = new BABYLON.Engine(canvas, true);
     const scene = createScene(canvas, engine);
     $("#leave-button").on('click', leave_room);
     player_id = get_cookie("player_id");
     room_id = get_cookie("room_id");
-    $('#room_id').html("Room ID: " + room_id);
     socket = io.connect(SERVER_ADDRESS.IP + ':' + SERVER_ADDRESS.PORT);
     socket.emit('joinroom', {player_id: player_id, room_id: room_id});
     socket.on('joinroom_message', (data) => {
@@ -226,7 +209,6 @@ function initialize() {
     socket.on('gamestart_message', (data) => {
         $('#message').html(data.message);
         currentPiece = data.piece_map[player_id];
-        opponentPiece = currentPiece == 'X' ? 'O' : 'X';
         run();
     });
 }
