2e902309877247250441809bc9c4c3af49dfe4a9
zhanqideng <zhanqi.deng@yale.edu>
2023-12-09 18:11:39 -0500

    able to specify port

--- back-end/run_server.py
+++ back-end/run_server.py
@@ -1,8 +1,17 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+
+import argparse
 from api_services import socketio, app
 
+def get_args():
+    """Parse arguments from command line input"""
+    parser = argparse.ArgumentParser(description='InstaGame 2023.'
+                                     , allow_abbrev=False)
+    parser.add_argument('port', type=int,
+                        help='the port at which the server should listen')
 
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
+    args = get_args()
+    port = args.port
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
